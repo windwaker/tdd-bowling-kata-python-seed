e379d1d2882adf6baad2f151deacce89c3b2044b
windwaker <windwaker@users.noreply.github.com>
2017-03-21 21:20:04 +0000

    Adding impl of first test

--- main/game_engine.py
+++ main/game_engine.py
@@ -1,5 +1,11 @@
 class Game:
 
+    def __init__(self):
+        self._score = 0
+
     def roll(self, pins_knocked):
+        self._score = pins_knocked
 
     def score(self):
+        return self._score
+
