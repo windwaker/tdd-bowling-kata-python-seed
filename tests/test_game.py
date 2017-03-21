fe391d4c48dea13a48258469c844a871394f15a6
windwaker <windwaker@users.noreply.github.com>
2017-03-21 21:21:14 +0000

    Adding impl of first test

--- tests/test_game.py
+++ tests/test_game.py
@@ -2,4 +2,13 @@ import pytest
 from main.game_engine import Game
 
 
-class Tests:
+def test_worst_player():
+    game = Game()
+
+    for counter in range(1, 20):
+        game.roll(0)
+
+    assert game.score() == 0
+
+
+
